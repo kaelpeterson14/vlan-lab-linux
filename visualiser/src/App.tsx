2ef81eb60dbc1ad6f0590c433bf12bdd8f697f76
devingupta <u1475642@utah.edu>
2026-02-18 13:13:56 -0700

    chore: clean up files

--- visualiser/src/App.tsx
+++ visualiser/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <div className="w-screen h-screen bg-gray-950 text-white flex items-center justify-center">
-      <h1 className="text-2xl font-mono">Kael is the best (spinach better)</h1>
+      <h1 className="text-2xl font-mono">Kael is late (spinach better)</h1>
     </div>
   )
 }
